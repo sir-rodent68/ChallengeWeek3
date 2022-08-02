15f7eef0ee143a4b6fa069bdba351ae9c66acdbd
sir_rodent <sirrodent68@gmail.com>
2022-08-02 14:31:53 -0600

    added function to generate a random password

--- 02-Challenge/Develop/script.js
+++ 02-Challenge/Develop/script.js
@@ -1,11 +1,23 @@
 // Assignment Code
 var generateBtn = document.querySelector("#generate");
-
+var alphabet = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
 // gives a random number between to given numbers
 function random(min, max) {
   const num = Math.floor(Math.random() * (max - min + 1)) + min;
   return num;
 };
+
+// generates a random passord
+function generatePassword() {
+  var passLength = random(8, 24);
+  var password = "";
+  for (var i = 0; i < passLength; i++) {
+    var passChar = random(0, 9);
+    password = password + passChar;
+  };
+
+  return(password)
+}
 
 // Write password to the #password input
 function writePassword() {
