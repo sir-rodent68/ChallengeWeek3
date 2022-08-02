ed7c4ab07fc1efe95b699e12583589a95dd45805
sir_rodent <sirrodent68@gmail.com>
2022-08-02 14:40:23 -0600

    added random letters into password generation

--- 02-Challenge/Develop/script.js
+++ 02-Challenge/Develop/script.js
@@ -1,6 +1,9 @@
 // Assignment Code
 var generateBtn = document.querySelector("#generate");
+
+// an array to assign each letter of the alphabet a number
 var alphabet = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
+
 // gives a random number between to given numbers
 function random(min, max) {
   const num = Math.floor(Math.random() * (max - min + 1)) + min;
@@ -11,8 +14,15 @@ function random(min, max) {
 function generatePassword() {
   var passLength = random(8, 24);
   var password = "";
+
   for (var i = 0; i < passLength; i++) {
-    var passChar = random(0, 9);
+    var passChar = random(0, 35);
+
+    if (passChar > 9) {
+      passChar = passChar - 9;
+      passChar = alphabet[passChar]
+    }
+    console.log(passChar)
     password = password + passChar;
   };
 
